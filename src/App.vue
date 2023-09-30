b28449f4f00e97928e6ea515a72972c27731e5aa
José Gil <josegil1909@gmail.com>
2023-09-30 17:49:56 -0400

    Arreglado algunos bugs

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <main>
+
+    <h1 class="text-3xl font-bold underline text-yellow-300">
+            Estrellitas!
+        </h1>
+    <div>
+
+      <Formulario v-for="(dataItem, index) in data" :key="index" :clave="index" :ropa="dataItem.ropa" :nota="dataItem.nota"
+      :marca="dataItem.marca" @retorno="retorno" />
+    </div>
+  </main>
+</template>
+
+<script setup>
+import Formulario from './components/Formulario.vue';
+import { ref, computed } from 'vue';
+
+const retorno = (a,i)=>{
+  if(a > 5){
+    return data.value[i].nota = 1;
+  }
+  data.value[i].nota = a;
+
+}
+
+const data = ref([
+  {
+    ropa: "camiseta",
+    marca: "adidas",
+    nota: 5,
+  },
+  {
+    ropa: "pantalon",
+    marca: "nike",
+    nota: 4
+  },
+  {
+    ropa: "zapatos",
+    marca: "adidas",
+    nota: 3,
+  }
+]);
+
+let id = ref(0);
+
+const index = ref(id)
+
+
+const aumentar = computed(() => {
+  id.value++;
+});
+
+const ropa = computed(() => {
+  return data[id.value].ropa;
+});
+const marca = computed(() => {
+  return data[id.value].marca
+});
+
+
+const nota = computed(() => {
+  if (data[id.value].nota > 5) {
+    return (data[id.value].nota = id.value);
+  }
+  return data[id.value].nota;
+});
+
+</script>
+
+<style scoped></style>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,55 @@
+<template >
+    <div class="bg-gray text-black rounded">
+
+        <form action="retorno" @submit.prevent>
+            <div class="flex flex-row space-x-5 text-xl mt-5 ">
+                <div class="basis-1/2 bg-red-500 rounded py-2 px-4 text-center"
+                    style="background-color: rgb(184, 184, 184); ">
+                    <span class="font-bold" for="ropa">({{ ropa }}) </span>
+                    <span class="" for="ropa"> &nbsp;{{ marca }}</span>
+
+                </div>
+                <div class=" grid grid-cols-5   bg-blue-500 rounded py-2 px-4 text-center">
+                    <!-- <label for="ropa">Estrellas</label> -->
+                    <img v-for="(notas, index) in nota" src="/estrellita.png" height="50" width="30">
+
+                </div>
+                <button @click="incrementarNota(nota, clave)" class="basis-1/10 bg-slate-500 rounded py-2 px-4 text-center"
+                    type="submit">+</button>
+            </div>
+
+        </form>
+    </div>
+</template>
+
+
+<script setup>
+import { ref, computed } from 'vue';
+
+
+
+const emit = defineEmits(['retorno']);
+
+const incrementarNota = (a, i) => {
+    a++;
+
+    emit('retorno', a, i);
+}
+
+const props = defineProps({
+    marca: String,
+    ropa: String,
+    nota: Number,
+    clave: Number,
+
+})
+
+
+
+
+
+
+</script>
+
+
+<style scoped></style>
